1f136a51b60d8d9911f1b09cf950f83d216b71ae
ambsenyestudi <adrifre7@hotmail.com>
2021-06-12 09:58:45 +0200

    Not capture pawn in front red

--- src/ChessBishopAndPawn/ChessBishopAndPawn.Test/CaptureShould.cs
+++ src/ChessBishopAndPawn/ChessBishopAndPawn.Test/CaptureShould.cs
@@ -16,5 +16,15 @@ namespace ChessBishopAndPawn.Test
             Assert.True(sut.IsCapture(pawnPostions));
 
         }
+        [Fact]
+        public void NotCapturePawnInFront()
+        {
+            var bishopPostions = new PiecePostition('H', 1);
+
+            var pawnPostions = new PiecePostition('H', 3);
+            var sut = new PieceCapture(bishopPostions);
+            Assert.False(sut.IsCapture(pawnPostions));
+
+        }
     }
 }
